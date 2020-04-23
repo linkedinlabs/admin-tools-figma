a1f99a64baf8257d845c61ad3cadd2404fad036e
Grant Blakeman <hello@grantblakeman.com>
2020-04-23 16:40:15 -0700

    fix build

--- src/GUI.ts
+++ src/GUI.ts
@@ -1,5 +1,5 @@
 import './assets/css/main.scss';
-import App from './views/App';
+import App from './views/App.svelte'; // eslint-disable-line import/extensions
 
 const app = new App({
   target: document.body,
