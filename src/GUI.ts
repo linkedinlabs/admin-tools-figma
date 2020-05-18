a53f2855797df9beeef98f06360d6c4be9e86ecb
Grant Blakeman <hello@grantblakeman.com>
2020-05-18 14:52:38 -0700

    ui - hook in selection watcher

--- src/GUI.ts
+++ src/GUI.ts
@@ -5,7 +5,7 @@ const app = new App({
   target: document.body,
   props: {
     inspect: 'styles', // 'styles' or 'components'
-    numberSelected: 18,
+    numberSelected: 0,
   },
 });
 
@@ -25,10 +25,75 @@ const sendLoadedMsg = (): void => {
   return null;
 };
 
+/* process Messages from the plugin */
+
+/** WIP
+ * @description Clones a template html element and then updates the clone’s contents to match
+ * the supplied options for each layer in the supplied array.
+ *
+ * @kind function
+ * @name updateSelected
+ *
+ * @param {Array} layers An array of layers to clone. Each entry should include an `id`,
+ * an `assignment`, `originalText`, `proposedText`, and a `locked` boolean.
+ *
+ * @returns {null}
+ */
+const updateSelected = (layers: Array<{
+  id: string,
+  assignment: string,
+  originalImage: Uint8Array,
+  originalText: string,
+  proposedText: string,
+  nodeType: 'text' | 'shape',
+  rounded: 'all' | 'none' | 'some',
+  locked: boolean,
+}>): void => {
+  const layerCount = layers.length;
+  app.numberSelected = layerCount || 0;
+};
+
+/** WIP
+ * @description Watches for incoming messages from the plugin’s main thread and dispatches
+ * them to the appropriate GUI actions.
+ *
+ * @kind function
+ * @name watchIncomingMessages
+ *
+ * @returns {null}
+ */
+const watchIncomingMessages = (): void => {
+  onmessage = ( // eslint-disable-line no-undef
+    event: {
+      data: {
+        pluginMessage: {
+          action: string,
+          payload: any,
+        }
+      }
+    },
+  ) => {
+    const { pluginMessage } = event.data;
+
+    switch (pluginMessage.action) {
+      case 'refreshState':
+        updateSelected(pluginMessage.payload);
+        break;
+      // case 'resetState':
+      //   setButtonState('ready');
+      //   break;
+      default:
+        return null;
+    }
+
+    return null;
+  };
+};
+
 // init GUI
 window.app = app; // eslint-disable-line no-undef
 // watchActions();
-// watchIncomingMessages();
+watchIncomingMessages();
 sendLoadedMsg();
 
 export default app;
--- src/views/App.svelte
+++ src/views/App.svelte
@@ -1,16 +1,29 @@
 <script>
+  import { isStyles } from './stores';
   import BlankState from './BlankState';
   import FontPreload from './FontPreload';
   import SampleOptions from './SampleOptions';
   import SceneNavigator from './SceneNavigator';
   import StatusBar from './StatusBar';
 
+  export let inspect = 'components';
   export let numberSelected;
+
+  const setIsStyles = (currentlyInspecting) => {
+    const isStylesLocal = currentlyInspecting === 'styles';
+    isStyles.set(isStylesLocal);
+  };
+
+  setIsStyles(inspect);
 </script>
 
+<!-- compile options -->
+<svelte:options accessors={true}/>
+
+<!-- core layout -->
 <div>
   <FontPreload/>
-  <SceneNavigator/>
+  <SceneNavigator />
 
   {#if numberSelected > 0}
     <SampleOptions numberSelected={numberSelected}/>
