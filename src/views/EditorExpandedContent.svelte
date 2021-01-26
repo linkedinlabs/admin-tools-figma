2648e9e823e3b433fb8b483d22ba7498b67c8ecf
Grant Blakeman <hello@grantblakeman.com>
2021-01-26 13:34:15 -0700

    ui - bulk editor - add warning icon

--- src/views/EditorExpandedContent.svelte
+++ src/views/EditorExpandedContent.svelte
@@ -5,6 +5,7 @@
   import Description from './Description';
   import FormActions from './forms-controls/FormActions';
   import FormUnit from './forms-controls/FormUnit';
+  import IconInline from './IconInline';
   import {
     checkFilterMatch,
     deepCopy,
@@ -107,7 +108,21 @@
 
 <section class="expanded-content editor">
   <span class="divider-top"><hr class="inner"></span>
-  <p class="banner-text"><strong>WARNING:</strong> Fields marked with <span class="fa fa-exclamation-circle warning"/> have existing values in your selection that will be overridden.</p>
+  {#if editorItem.overrides && editorItem.overrides.length > 0}
+    <p class="banner-text">
+      <strong>WARNING:</strong>
+      Fields marked with
+      <IconInline
+        invertView={true}
+        isWarning={true}
+      >
+        <svg viewBox="0 0 512 512">
+          <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path>
+        </svg>
+      </IconInline>
+      have existing values in your selection that will be overridden if changed.
+    </p>
+  {/if}
   <span class="form-element-holder">
     {#if $isStyles || checkFilterMatch($currentFilter, 'all-components')}
       <span class="form-row">
